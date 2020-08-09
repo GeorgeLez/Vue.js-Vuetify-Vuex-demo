52682ca0368175c39336c95718a1d79074440e46
GeorgeLez <llg.george@yahoo.com>
2020-08-09 11:32:33 +0300

    Update Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,7 +5,7 @@
     <!-- <v-snackbar :value="valueOfSnack" @input="snackSwitch" :timeout="2000" top color="success"> -->
     <v-snackbar v-model="snackbarProccessed" :timeout="6000" top color="success">
       <div class="d-flex align-center justify-space-around">
-        <h3>Greetings, {{getDisplayName}}.</h3>
+        <h3>Greetings, {{getDisplayName}}</h3>
         <v-btn class="ml-3" text color="white" @click="snackbar = false">Close</v-btn>
       </div>
     </v-snackbar>
